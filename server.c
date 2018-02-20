68257effe7faf969b307f371b7f25d9fbb127189
Tim Pugh <timpugh@pdx.edu>
2018-02-20 14:45:39 -0800

    Success sending a text file, except for need to size the write correctly..

--- server.c
+++ server.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <pthread.h>
 #include <unistd.h>
+//#include <netdb.h>
+//#include <sys/types.h>
+//#include <sys/socket.h>
+//#include <pthread.h>
 
-void main(int argc, char * argv[]) {
+
+int main(int argc, char * argv[]) {
     if(argc < 2){
         printf("Not enough arguments. System exiting..");
         exit(0);
     }
+
     //socket creation
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
     if(sock < 0) printf("Could not open socket!!\n");
@@ -35,12 +36,32 @@ void main(int argc, char * argv[]) {
     if((file = fopen(buffer, "r"))) {
          printf("%s", "File exists!\n");
          memset(&buffer, 0, sizeof buffer);
-         strcpy(buffer, "File exists! Preparing transmission..\n");
-         sendto(sock, buffer, 1024,0,(struct sockaddr*)&from, fromLength);
+        // strcpy(buffer, "File exists! Preparing transmission..\n");
+         //sendto(sock, buffer, 1024,0,(struct sockaddr*)&from, fromLength);
     }
     else {
          printf("%s", "File does not exists. Exiting..");
          exit(0);
     }
+
+   int stillReading;
+    while(1) {
+        memset(&buffer, 0, sizeof buffer);
+        stillReading = fread(buffer,sizeof(buffer),1,file);
+
+        if(stillReading == 0){
+            memset(&buffer, 0, sizeof buffer);
+            strcpy(buffer, "Finished");
+            sendto(sock, buffer, 1024, 0, (struct sockaddr*)&from, fromLength);
+            break;
+        }
+
+        sendto(sock, buffer, 1024, 0, (struct sockaddr*)&from, fromLength);
+    }
+
+    fflush(file);
+    fclose(file);
+    close(sock);
+    return 0;
 }
 
